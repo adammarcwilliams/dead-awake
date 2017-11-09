079ec7240f0955a11fdb462b7c62325abbfba517
Adam Marc Williams <adam@adammarcwilliams.co.uk>
2017-11-09 22:50:18 +0000

    Adjust lighting and adds atmospheric particles + post processing

---
+++ Assets/_scripts/LightFlicker.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlicker : MonoBehaviour {
+
+	[SerializeField] float m_range = 0.3f;
+	Light m_light;
+
+	// Use this for initialization
+	void Start () {
+		m_light = GetComponent<Light>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		m_light.intensity = Mathf.PingPong(Time.time * Random.Range(0f, 4f), Random.Range(0f, m_range));
+	}
+}
